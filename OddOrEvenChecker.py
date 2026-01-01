122e0f6a2be89e6abdb819681a7c2e7f1a66bf96
Mugunthan <smugunthan790@gmail.com>
2026-01-01 19:04:05 +0530

    odd or even finder using class and object

---
+++ OddOrEvenChecker.py
@@ -0,0 +1,13 @@
+class oe:
+    def getdata(self):
+        self.num=int(input("Enter the number:"))
+        
+    def process(self):
+        if self.num%2==0:
+            print(f"{self.num} is Even Number")
+        else:
+            print(f"{self.num} is Odd Number")
+            
+num1=oe()
+num1.getdata()
+num1.process()
