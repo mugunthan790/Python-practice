509fe58097ef714df3ed9ccc5c615a0d211c1592
Mugunthan <smugunthan790@gmail.com>
2026-01-02 19:51:54 +0530

    find largest of three numbers using class and object

---
+++ FindThreeLargestNumbeer.py
@@ -0,0 +1,22 @@
+try:
+ class fln:
+    def getdata(self):
+        self.n1=int(input("Enter 1st number:"))
+        self.n2=int(input("Enter 2nd number:"))
+        self.n3=int(input("Enter 3rd number:"))
+        
+    def process(self):
+        if self.n1>self.n2 and self.n3:
+            print(f"{self.n1} is grater then {self.n2} and {self.n3}")
+        elif self.n2>self.n3 and self.n1:
+            print(f"{self.n2} is grater then {self.n1} and {self.n3}")
+        elif self.n3>self.n1 and self.n2:
+            print(f"{self.n3} is grater then {self.n1} and {self.n2}")
+            
+ num=fln()
+ num.getdata()
+ num.process()
+except Exception as e:
+    print("invalid",e)
+finally:
+    print("done")
