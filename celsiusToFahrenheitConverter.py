a35940be2d860b3f2f1532922610320bca30a45e
Mugunthan <smugunthan790@gmail.com>
2026-01-02 20:05:29 +0530

    celsius to fahrenhite conveter

--- celsiusToFahrenheitConverter.py
+++ celsiusToFahrenheitConverter.py
@@ -1,11 +1,16 @@
-class temp():
+try:
+ class temp():
     def getdata(self):
         self.s=int(input("Enter Celsius Tempracture:"))
     def cal(self):
         self.f=(self.s*9/5)+32
     def display(self):
         print(f"the fahrenhite of {self.s} celsius is : {self.f}")
-find=temp()
-find.getdata()
-find.cal()
-find.display()
+ find=temp()
+ find.getdata()
+ find.cal()
+ find.display()
+except Exception as e:
+    print(f"invalid input {e}")
+finally:
+    print("done")
