f72bd04e0cd772dee8a184aae688a6b5d8a6eb44
Mugunthan <smugunthan790@gmail.com>
2026-01-05 20:19:44 +0530

    reverse the input

--- reverseNum.py
+++ reverseNum.py
@@ -1,11 +1,16 @@
-class rev:
+try:
+ class rev:
     def getdata(self):
         self.num=(input("Enter the number:"))
     def process(self):
         self.reverse=self.num[::-1]
     def display(self):
         print(f"reverse num is : {self.reverse}")
-num1=rev()
-num1.getdata()
-num1.process()
-num1.display()
+ num1=rev()
+ num1.getdata()
+ num1.process()
+ num1.display()
+except Exception as e:
+    print(f"invalid option {e}")
+finally:
+    print("exit")
