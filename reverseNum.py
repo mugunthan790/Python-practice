3b81bdc5b0afea7827b19460b8b415e4d2d60325
Mugunthan <smugunthan790@gmail.com>
2026-01-05 20:15:39 +0530

    reverse number

---
+++ reverseNum.py
@@ -0,0 +1,11 @@
+class rev:
+    def getdata(self):
+        self.num=(input("Enter the number:"))
+    def process(self):
+        self.reverse=self.num[::-1]
+    def display(self):
+        print(f"reverse num is : {self.reverse}")
+num1=rev()
+num1.getdata()
+num1.process()
+num1.display()
