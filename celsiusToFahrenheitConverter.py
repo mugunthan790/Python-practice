6bda8a7b5940aebb0e1949927e3247f7fca98bb7
Mugunthan <smugunthan790@gmail.com>
2026-01-07 20:27:19 +0530

    celsius to fahrenhite conveter

--- celsiusToFahrenheitConverter.py
+++ celsiusToFahrenheitConverter.py
@@ -1,11 +1,11 @@
 try:
  class temp():
     def getdata(self):
-        self.s=int(input("Enter Celsius Tempracture:"))
+        self.c=int(input("Enter Celsius Tempracture:"))
     def cal(self):
-        self.f=(self.s*9/5)+32
+        self.f=(self.c*9/5)+32
     def display(self):
-        print(f"the fahrenhite of {self.s} celsius is : {self.f}")
+        print(f"the fahrenhite of {self.c} celsius is : {self.f}")
  find=temp()
  find.getdata()
  find.cal()
