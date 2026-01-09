62577313bbaae7d9fb98bf2b6e7b3866b22d0c4d
Mugunthan <smugunthan790@gmail.com>
2026-01-09 20:50:22 +0530

    maths tables

--- MathTables.py
+++ MathTables.py
@@ -1,10 +1,10 @@
 class tables:
     def getdata(self):
-        self.num=int(input("Enter the number:"))
+        self.n=int(input("Enter the number:"))
         
     def process(self):
         for i in range(1,11):
-          print(f"{i}*{self.num}={i*self.num}")
+          print(f"{i}*{self.n}={i*self.n}")
           
 execute=tables()
 execute.getdata()
