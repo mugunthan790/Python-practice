de25443c19b6cd23fea3262b2ffecfb2cbc5c1d1
Mugunthan <smugunthan790@gmail.com>
2026-01-12 19:23:42 +0530

    negetavie or positive finder

--- positiveOrNegativeFinder.py
+++ positiveOrNegativeFinder.py
@@ -1,15 +1,15 @@
 try:
  class pnf():
     def getdata(self):
-        self.num=int(input("Enter the number:"))
+        self.n=int(input("Enter the number:"))
         
     def process(self):
-        if self.num>0:
-            print(f"{self.num} is Positive Number")
-        elif self.num==0:
-            print(f"{self.num} is Zero")
+        if self.n>0:
+            print(f"{self.n} is Positive Number")
+        elif self.n==0:
+            print(f"{self.n} is Zero")
         else:
-            print(f"{self.num} is Negetive Number")
+            print(f"{self.n} is Negetive Number")
             
  find=pnf()
  find.getdata()
