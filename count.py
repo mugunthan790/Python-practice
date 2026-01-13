2843750f7110114b7a7629f1e0c868e566219630
Mugunthan <smugunthan790@gmail.com>
2026-01-13 19:26:21 +0530

    count in python

---
+++ count.py
@@ -0,0 +1,13 @@
+class count():
+    def getdata(self):
+       self.num=int(input("enter the number:"))
+    def process(self):
+        self.count=0
+        for self.i in range(1,self.num+1):
+         if self.i%2==0:
+          print(self.i)
+          self.count=self.count+1
+        print(f"count={self.count}")
+count1=count()
+count1.getdata()
+count1.process()
