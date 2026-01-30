d69b20c53cd6943aa85e09c6181bbc472f8a8057
Mugunthan <smugunthan790@gmail.com>
2026-01-30 19:08:41 +0530

    find largest number using array

---
+++ FindLargestNumberInArray.py
@@ -0,0 +1,18 @@
+class largestnum:
+    def getdata(self):
+      self.num=[]
+      for i in range(1,4):
+        self.num.append(int(input(f"Enter the number {i}:")))
+      print(self.num)
+    def process(self):
+     if self.num[0]>self.num[1]and self.num[0]>self.num[2]:
+      print(f"{self.num[0]} is largest")
+     elif self.num[1]>self.num[0] and self.num[1]>self.num[2]:
+      print(f"{self.num[1]} is largest")
+     else:
+      print(f"{self.num[2]} is largest")
+      
+n=largestnum()
+n.getdata()
+n.process()
+      
