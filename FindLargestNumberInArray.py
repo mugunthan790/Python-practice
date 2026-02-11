c5608f2a52a885d86083b415518ba25a04b22067
mugunthan790 <smugunthan790@gmail.com>
2026-02-11 23:18:05 +0530

    Update FindLargestNumberInArray.py

--- FindLargestNumberInArray.py
+++ FindLargestNumberInArray.py
@@ -3,7 +3,7 @@ try:
     def getdata(self):
       self.num=[]
       for i in range(1,4):
-        self.num.append(int(input(f"Enter the number {i}:")))
+        self.num.append(int(input(f"Enter the Number {i}:")))
       print(self.num)
     def process(self):
      if self.num[0]>self.num[1]and self.num[0]>self.num[2]:
