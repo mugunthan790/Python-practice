3d204bfaa251fc7571e45306226d70a494e8d830
mugunthan790 <smugunthan790@gmail.com>
2026-02-11 23:18:57 +0530

    Update largest&smallestNum.py

--- array codes/largest&smallestNum.py
+++ array codes/largest&smallestNum.py
@@ -1,7 +1,7 @@
 a=[]
-n=int(input("enter the element:"))
+n=int(input("Enter The Element:"))
 for j in range(1,n+1):
-    a.append(int(input(f"Enter the number {j}:")))
+    a.append(int(input(f"Enter The Number {j}:")))
 print()
 print(a)
 print()
