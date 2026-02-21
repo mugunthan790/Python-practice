8b1e7f462af3d64a2255a59e72dc54ba8cb302f4
Mugunthan <smugunthan790@gmail.com>
2026-02-21 19:06:29 +0530

    removing duplicate values using arry

--- array codes/withoutDuplicate.py
+++ array codes/withoutDuplicate.py
@@ -1,7 +1,12 @@
-a=[]
-e=int(input("Enter the number of elements :"))
-for i in range(1,e+1):
+try:
+ a=[]
+ e=int(input("Enter the number of elements :"))
+ for i in range(1,e+1):
     a.append(int(input(f"enter the value {i} :")))
-print(a)
-unique_value=list(set(a))
-print(f"the output without  duplicate values: {unique_value}")
+ print(a)
+ unique_value=list(set(a))
+ print(f"the output without  duplicate values: {unique_value}")
+except Exception as s:
+    print(f"invalid input {s}")
+finally:
+    print("done")
