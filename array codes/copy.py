0360f1d4e882d7897cddf2e6ee1ac4627160fad1
mugunthan790 <smugunthan790@gmail.com>
2026-02-22 20:03:16 +0530

    Update copy.py

--- array codes/copy.py
+++ array codes/copy.py
@@ -1,5 +1,5 @@
 a=[]
-n=int(input("enter the number of elements:"))
+n=int(input("Enter The Number of Elements:"))
 for i in range (1,n+1):
     a.append(int(input(f"enter the value {i} :")))
 print(a)
