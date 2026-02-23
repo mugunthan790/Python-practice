920f6c9cf20789a3fd0aba00610d0af964754ac8
mugunthan790 <smugunthan790@gmail.com>
2026-02-23 20:56:00 +0530

    Update withoutDuplicate.py

--- array codes/withoutDuplicate.py
+++ array codes/withoutDuplicate.py
@@ -1,11 +1,11 @@
 try:
  a=[]
- e=int(input("Enter the Number of Elements :"))
+ e=int(input("Enter The Number Of Elements :"))
  for i in range(1,e+1):
-    a.append(int(input(f"enter the value {i} :")))
+    a.append(int(input(f"Enter The Value {i} :")))
  print(a)
  unique_value=list(set(a))
- print(f"the output without  duplicate values: {unique_value}")
+ print(f"The Output Without  Duplicate values: {unique_value}")
 except Exception as s:
     print(f"invalid input {s}")
 finally:
