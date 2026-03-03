15d9fb0df866a7933f96845a6322ef7d7ae5517c
mugunthan790 <smugunthan790@gmail.com>
2026-03-03 21:13:18 +0530

    Update copy.py

--- array codes/copy.py
+++ array codes/copy.py
@@ -1,7 +1,7 @@
 a=[]
 n=int(input("Enter The Number of Elements:"))
 for i in range (1,n+1):
-    a.append(int(input(f"Enter the value {i} :")))
+    a.append(int(input(f"Enter The value {i} :")))
 print(a)
 b=a.copy()
 print(f"The Copied Values {b}")
