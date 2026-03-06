aa38985da32a850d1800c130688dc72a49bea0d7
mugunthan790 <smugunthan790@gmail.com>
2026-03-06 22:52:02 +0530

    Update printArray.py

--- array codes/printArray.py
+++ array codes/printArray.py
@@ -1,7 +1,7 @@
 a=[]
 n=int(input("Enter The Elements:"))
 for i in range(1,n+1):
-    a.append(int(input(f"Enter the Number {i} :")))
+    a.append(int(input(f"Enter The Number {i} :")))
 print(a)
 rev=a[::-1]
 print(f"The Reversed String is {rev}")
